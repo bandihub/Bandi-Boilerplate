a708c107990a0bf5753a773d93ba54b5347af7fc
Elijah Bandi <imdatbulll@gmail.com>
2014-07-30 07:04:19 -0400

    update

--- economy.js
+++ economy.js
@@ -40,8 +40,41 @@ var ecomony = {
     
     
     casino: {
-    dice: {},
-    roulette: {},
+    dice: {
+        compareRolls: function (rolls, players, room) {
+                var winner = '';
+                var loser = '';
+                if (rolls[Users.users[players[0]]] > rolls[Users.users[players[1]]]) {
+                    winner = Users.users[players[0]].userid;
+                    loser = Users.users[players[1]].userid;
+                } else {
+                    winner = Users.users[players[1]].userid;
+                    loser = Users.users[players[0]].userid;
+                }
+                if (!rolls[Users.users[players[1]] === rolls[Users.users[players[0]]]]) {
+                    room.addRaw(Users.users[players[0]].name + ' rolled a <font color=red>' + rolls[Users.users[players[0]]] + '</font>');
+                    room.addRaw(Users.users[players[1]].name + ' rolled a <font color=red>' + rolls[Users.users[players[1]]] + '</font>');
+                    room.addRaw('<font color=#24678d> ' + winner + ' wins the dice game and ' + '<font color=red>' + dice[room.id].bet + '</font> bucks.</font>');
+
+                    var total = Number(Core.stdin('money.csv', Users.users[winner].userid)) + Number(dice[room.id].bet)
+                    Core.stdout('money.csv', Users.users[winner].userid, total);
+
+                    var amount = Number(Core.stdin('money.csv', Users.users[loser].userid)) - Number(dice[room.id].bet);
+                    Core.stdout('money.csv', Users.users[loser].userid, amount);
+                } else {
+                    return room.add('It was a draw, both frens keep their money');
+                }
+                delete this[room.id];
+            },
+            generateRolls: function (players, room) {
+                var facez = [1, 2, 3, 4, 5, 6];
+                for (var i = 0; i < players.length; i++) {
+                    this[room.id].rolls[Users.users[players[i]]] = facez[Math.floor(Math.random() * 6)];
+                }
+        },
+    roulette: {
+            
+        },
     },
     
     arcade: {}
